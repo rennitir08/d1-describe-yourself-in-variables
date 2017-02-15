70b14601da50d12ad5c289760ed90aa32aa414d8
YOUR NAMEJennifer <YOUR EMAIL ADDRESSjenniferbonner12@gmail.com>
2017-02-14 22:18:02 -0500

    added js files

---
+++ js/about-me.js
@@ -0,0 +1,25 @@
+var firstName = "Jennifer";
+var lastName = "Bonner";
+var fullName = firstName + '' + lastName;
+var email = "jenniferbonner12@gmail.com"
+var losAngeles = true
+var college = 4.5
+var pets = 2
+var hoursSlept = 5.75
+var music = "acapella"
+var showMovies = ['twilight', 'star', 'gilmore girls']
+var jobTitle;
+var myLife = " "
+
+console.log (firstName)
+console.log(lastName)
+console.log(fullName)
+console.log(email)
+console.log(losAngeles)
+console.log(college)
+console.log(pets)
+console.log(hoursSlept)
+console.log(music)
+console.log(showMovies)
+console.log(jobTitle)
+console.log(myLife)
---
+++ js/madlibs.js
@@ -0,0 +1,25 @@
+var sun = "Lucas Oil Stadium"
+var shine = "fierce"
+var wet = "intimidating"
+var play = "skip"
+var sat = "flew"
+var house = "Ben Davis High School"
+var cold = "cool"
+var Sally = "Darrell"
+var two = "27"
+var something = "beautiful"
+var wet = "sexy"
+var out = "blue"
+var cold = "annoying"
+var ball = "basketball"
+var house = "Dept. of Homeland Security"
+var sit = "kick"
+var like = "punch"
+var bit = "yellow"
+var bump = "shooting"
+var jump = "carry"
+
+var story = `The ${sun} did not ${shine}. It was too ${wet} to ${play}.So we ${sat} in the ${house} all that ${cold}, cold, wet day.I ${sat} there with ${Sally}.We sat there, we ${two}. And i said, 'how I wish we had ${something} to do!'Too ${wet} to go ${out} and too ${cold} to play ${ball}.So we sat in the ${house}.We did nothing at all.So all we could do was to ${sit}! ${sit}! ${sit}! ${sit}! And we did not ${like} it. not one little ${bit}. And then something went ${bump}! How that bump made us ${jump}! We looked! Then we saw him step in on the mat! We looked! And we saw him! The cat in the hat! And he said to us,'Why do you sit there like that?''I know it is wet and the sun is not sunny. But we can have lots of good fun that is funny!'`;
+
+console.log(story)
+
